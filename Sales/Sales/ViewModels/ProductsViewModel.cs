f20b80b1ca270df6d712ebf132fe8b99df102960
Bladimir Velásquez <bladi135@gmail.com>
2019-02-28 01:46:47 -0400

    Refresh Listview Products
    Activity Indicator Refresh LisstView Product

--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -3,17 +3,22 @@
 
 namespace Sales.ViewModels
 {
+    using GalaSoft.MvvmLight.Command;
     using Sales.Common.Models;
     using Sales.Services;
     using System;
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
+    using System.Windows.Input;
     using Xamarin.Forms;
 
-   public class ProductsViewModel : BaseViewModel
+    public class ProductsViewModel : BaseViewModel
     {
-        private ApiService apiService;
+        private readonly ApiService apiService;
         private ObservableCollection<Product> products;
+
+        private bool isRefreshing;
+
 
         public ObservableCollection<Product> Products
         {
@@ -29,17 +34,32 @@ namespace Sales.ViewModels
 
         private async void LoadProducts()
         {
-            var response = await this.apiService.GetList<Product>("http://192.168.1.201/","/sales.api/api","/Products");
+            this.IsRefreshing = true;
+
+            var response = await this.apiService.GetList<Product>("http://192.168.1.201/", "/sales.api/api", "/Products");
             if (!response.IsSuccess)
             {
                 await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
                 return;
             }
-            
+
             var list = (List<Product>)response.Result;
             this.Products = new ObservableCollection<Product>(list);
 
+            this.IsRefreshing = false;
+
             //await Application.Current.MainPage.DisplayAlert("Data", list[0].Description, "Accept");
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { this.SetValue(ref this.isRefreshing, value); }
+        }
+
+        public ICommand RefreshCommand
+        {
+            get { return new RelayCommand(LoadProducts); }
         }
     }
 }
