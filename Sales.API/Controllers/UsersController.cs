c2c82b5ea74d9bacb7b5ae0bd90c1739d508b458
Bladimir Velásquez <bladi135@gmail.com>
2019-03-07 16:23:42 -0400

    Register User API Logic Implemented
    Register User API Logic Implemented

---
+++ Sales.API/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿namespace Sales.API.Controllers
+{
+    using System;
+    using System.IO;
+    using System.Web.Http;
+    using Common.Models;
+    using Helpers;
+    using Newtonsoft.Json.Linq;
+
+    [RoutePrefix("api/Users")]
+    public class UsersController : ApiController
+    {
+        public IHttpActionResult PostUser(UserRequest userRequest)
+        {
+            if(userRequest.ImageArray!=null && userRequest.ImageArray.Length > 0)
+            {
+                var stream = new MemoryStream(userRequest.ImageArray);
+                var guid = Guid.NewGuid().ToString();
+                var file = $"{guid}.jpg";
+                var folder = "~/Content/Users";
+                var fullPath = $"{folder}/{file}";
+                var response = FilesHelper.UploadPhoto(stream, folder, file);
+
+                if (response)
+                {
+                    userRequest.ImagePath = fullPath;
+                }
+            }
+
+            var answer = UsersHelper.CreateUserASP(userRequest);
+
+            if (answer.IsSuccess)
+            {
+                return Ok(answer);
+            }
+
+            return BadRequest(answer.Message);
+        }
+
+        /*   [HttpPost]
+           [Authorize]
+           [Route("GetUser")]
+
+           public IHttpActionResult GetUser(Object form)
+           {
+               try
+               {
+
+               }
+               catch (Exception)
+               {
+
+                   throw;
+               }
+           } */
+    }
+}
---
+++ Sales.API/Helpers/UsersHelper.cs
@@ -0,0 +1,118 @@
+﻿namespace Sales.API.Helpers
+{
+    using System;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using System.Web.Configuration;
+    using Sales.Common.Models;
+    using Sales.Domain.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+
+    public class UsersHelper : IDisposable
+    {
+        private static ApplicationDbContext userContext = new ApplicationDbContext();
+        private static DataContext db = new DataContext();
+
+        public static bool DeleteUser(string userName,string roleName)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+            var userASP = userManager.FindByEmail(userName);
+
+            if (userASP == null)
+            {
+                return false;
+            }
+
+            var response = userManager.RemoveFromRole(userASP.Id, roleName);
+            return response.Succeeded;
+        }
+
+        public static ApplicationUser GetUserASP(string email)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+            var userASP = userManager.FindByEmail(email);
+
+            return userASP;
+        }
+
+        public static Response CreateUserASP(UserRequest userRequest)
+        {
+            try
+            {
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(userContext));
+                var oldUserASP = userManager.FindByEmail(userRequest.EMail);
+                if (oldUserASP != null)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = "001. User already exists."
+                    };
+                }
+                var userASP = new ApplicationUser
+                {
+                    Email = userRequest.EMail,
+                    UserName = userRequest.EMail,
+                    PhoneNumber = userRequest.Phone,
+                };
+
+                var result = userManager.Create(userASP, userRequest.Password);
+                if (result.Succeeded)
+                {
+                    var newUserASP = userManager.FindByEmail(userRequest.EMail);
+                    userManager.AddClaim(newUserASP.Id, new Claim(ClaimTypes.GivenName, userRequest.FirstName));
+                    userManager.AddClaim(newUserASP.Id, new Claim(ClaimTypes.Name, userRequest.LastName));
+
+                    if (!string.IsNullOrEmpty(userRequest.Address))
+                    {
+                        userManager.AddClaim(newUserASP.Id, new Claim(ClaimTypes.StreetAddress, userRequest.Address));
+                    }
+
+                    if (!string.IsNullOrEmpty(userRequest.ImagePath))
+                    {
+                        userManager.AddClaim(newUserASP.Id, new Claim(ClaimTypes.Uri, userRequest.ImagePath));
+                    }
+
+                    return new Response
+                    {
+                        IsSuccess = true
+                    };
+
+                }
+
+                var errors = string.Empty;
+                foreach (var error in result.Errors)
+                {
+                    errors += $"{error}, ";
+                }
+
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = errors,
+                };
+
+            }
+            catch (Exception ex)
+            {
+
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                };
+            }
+           
+        }
+
+        //Pendiente UpdateUserName
+
+        public void Dispose()
+        {
+            userContext.Dispose();
+            db.Dispose();
+        }
+    }
+}
